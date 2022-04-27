96e363209b5e49a251897b752bbd6e6fd2c8182c
Luiza Schmidt Mescolotto <lsmescolotto@gmail.com>
2022-04-26 23:21:44 -0300

    feat: flask httpauth autentication completed

--- app/controllers/user_controller.py
+++ app/controllers/user_controller.py
@@ -1,9 +1,8 @@
 from http import HTTPStatus
 from flask import current_app, request, jsonify
-from app.models.user_model import UserModel
+from app.models.user_model import UserModel, auth
 from app.configs.database import db
 from sqlalchemy.exc import NoResultFound
-from flask_jwt_extended import jwt_required, create_access_token, get_jwt_identity
 
 
 def register_user():
@@ -11,41 +10,47 @@ def register_user():
 
     password_to_hash = data.pop("password")
 
-    data = data
-
     new_user = UserModel(**data)
     new_user.password = password_to_hash
-
-    db.session.add(new_user)
+    print(new_user)
 
     db.session.add(new_user)
     db.session.commit()
 
-    return jsonify(new_user), HTTPStatus.CREATED
+    return jsonify(
+        {"name": new_user.name, 
+        "last_name": new_user.last_name, 
+        "email": new_user.email}
+        ), HTTPStatus.CREATED
 
 
 def login():
     try:
         data = request.get_json()
-
+        
         found_user = UserModel.query.filter_by(email=data["email"]).one()
+        print(found_user)
         if not found_user.verify_password(data["password"]):
             return {"message": "Unauthorized"}, HTTPStatus.UNAUTHORIZED
-        access_token = create_access_token(identity=data["email"])
-        return {"access_token": f'{access_token}'}, HTTPStatus.OK
+
+        return {"api_key": found_user.api_key}, HTTPStatus.OK
     
     except NoResultFound:
-        return {"msg": "user not found"}, HTTPStatus.NOT_FOUND
-        # If password doesn't match return {"message": "Unauthorized"}, HTTPStatus.UNAUTHORIZED
+        return {"message": "user not found"}, HTTPStatus.NOT_FOUND
+    
+    except:
+        return {"message": "Unauthorized"}, HTTPStatus.UNAUTHORIZED
 
 
 
-@jwt_required()
+@auth.login_required
 def update_user():
     try:
         data = request.get_json()
 
-        user_to_update = UserModel.query.get(id)
+        print(auth.current_user())
+
+        user_to_update = auth.current_user()
 
         for key, value in data.items():
             setattr(user_to_update, key, value)
@@ -53,18 +58,48 @@ def update_user():
         db.session.add(user_to_update)
         db.session.commit()
 
-        return jsonify(user_to_update), HTTPStatus.OK
+        return jsonify(
+        {"name": user_to_update.name, 
+        "last_name": user_to_update.last_name, 
+        "email": user_to_update.email}
+        ), HTTPStatus.OK
+        
+    except NoResultFound:
+        return {"message": "user not found"}, HTTPStatus.NOT_FOUND
+    
+    except:
+        return {"message": "Unauthorized"}, HTTPStatus.UNAUTHORIZED
+
+
+@auth.login_required
+def delete_user():
+    try:
+        user_to_delete = auth.current_user()
+
+        current_app.db.session.delete(user_to_delete)
+        current_app.db.session.commit()
+
+        return "", HTTPStatus.NO_CONTENT
+    
+    except NoResultFound:
+        return {"message": "user not found"}, HTTPStatus.NOT_FOUND
+    
+    except:
+        return {"message": "Unauthorized"}, HTTPStatus.UNAUTHORIZED
+
+@auth.login_required
+def get_users():
+    try:
+        user_to_get = auth.current_user()
+        print(auth.current_user())
+        return jsonify(
+        {"name": user_to_get.name, 
+        "last_name": user_to_get.last_name, 
+        "email": user_to_get.email}
+        ), HTTPStatus.OK
 
     except NoResultFound:
-        return {"msg": "user not found"}, HTTPStatus.NOT_FOUND
-        # If password doesn't match return {"message": "Unauthorized"}, HTTPStatus.UNAUTHORIZED
-
-
-@jwt_required()
-def delete_user():
-    pass
-
-
-@jwt_required()
-def get_users():
-    pass
+        return {"message": "user not found"}, HTTPStatus.NOT_FOUND
+    
+    except:
+        return {"message": "Unauthorized"}, HTTPStatus.UNAUTHORIZED
